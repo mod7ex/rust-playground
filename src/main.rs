f0f692017c440afb3ebc26774e72fac856882c08
Mourad EL CADI <mouradface1998@gmail.com>
2023-03-23 07:37:41 +0000

    save

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,3 @@
 #![allow(unused)]
 
-#[derive(Default, Debug)]
-struct User {
-    name: String,
-    age: u8,
-}
-
-impl User {
-    fn new() -> User {
-        User{ ..Default::default() }
-    }
-
-}
-
-fn main() {
-    let me = User::new();
-
-    println!("{:?}", &me);
-}
+fn main() {}
